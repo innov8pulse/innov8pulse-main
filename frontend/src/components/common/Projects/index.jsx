bd61353f5b93e9f22b0a7aad91907aa37c14772f
kenkomu <komukenneth216@gmail.com>
2024-10-16 23:33:57 +0300

    added json file

--- frontend/src/components/common/Projects/index.jsx
+++ frontend/src/components/common/Projects/index.jsx
@@ -1,11 +1,34 @@
-// MainProjectsPage.js
 import React, { useState, useEffect } from 'react';
 import { getFirestore, collection, getDocs } from 'firebase/firestore';
 import { app } from '../../../firebaseConfig'; 
 import SearchProject from "../SearchProject";
+import AddProjectCard from "../AddProjectCard"; // Import the AddProjectCard
 import "../Projects/index.css";
+import { useAccount } from '@starknet-react/core'; // To check if the wallet is connected
+import { RpcProvider } from 'starknet';
+import abi from './MyContractAbi.json'; // Import ABI
+
+
 const MainProjectsPage = () => {
   const [projects, setProjects] = useState([]);
+  const { address } = useAccount(); // Get the connected wallet address
+ // Use the Provider class with the rpc URL
+ const provider = new RpcProvider({
+  nodeUrl: 'https://starknet-sepolia.public.blastapi.io/rpc/v0_7',
+});
+
+const privateKey = process.env.ACCOUNT_PRIVKEY;
+
+const accountAddress = '0x03553b785b4e9a6496118b6341c44700f209c60e50b8db7ef4ba8fb681a05cde';
+
+// Initialize the account
+const account = new Account(provider, accountAddress, privateKey);
+
+// Initialize deployed contract
+const testAddress = '0x29724d03151eff483f60b7f556593beb1f600bac9b5372240f924bc5b07fe18';
+
+// Connect the contract
+const myTestContract = new Contract(abi, testAddress, provider);
 
   useEffect(() => {
     const fetchProjects = async () => {
@@ -20,7 +43,11 @@ const MainProjectsPage = () => {
 
   return (
     <div>
-      < SearchProject />
+      <SearchProject />
+      
+      {/* Add the "Add Project" card if wallet is connected */}
+      {address && <AddProjectCard />}
+
       <div className="project-list">
         {projects.map((project) => (
           <div key={project.id} className="project-card">
